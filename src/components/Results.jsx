a80345757b8fafec5420d1581878cc61b941c5c6
NickAguilar93 <naguilar9404@conestogac.on.ca>
2024-08-19 15:54:02 -0400

    Added base visual components

---
+++ src/components/Results.jsx
@@ -0,0 +1,21 @@
+export default function Results() {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
